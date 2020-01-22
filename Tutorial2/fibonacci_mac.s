cc205d3b0a4b55a6fa09aeda01a276f95e98c88a
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2020-01-22 17:53:22 +0530

    Adding Code for Mac Users

---
+++ Tutorial2/fibonacci_mac.s
@@ -0,0 +1,24 @@
+# Compile using gcc -g -nostdlib Fibonacci.s
+# Calculate the Nth Fibonacci number
+
+.global _main
+
+.text
+
+_main:
+        mov $1, %rax         # a = 1
+        mov $2, %rbx         # b = 2  
+        mov $5, %rdx         # n = 5
+
+loop:
+    mov %rbx, %rcx           # temp = b  
+    add %rax, %rbx           # b = a + b 
+    mov %rcx, %rax           # a = temp
+    dec %rdx                 # n -= 1
+    cmp $0, %rdx             # Compare value of n and 0 
+    jne loop                 # if (n > 0), continue the loop 
+
+exit:
+        mov     $60, %rax               # system call 60 is exit
+        xor     %rdi, %rdi              # we want return code 0
+        syscall
---
+++ Tutorial2/odd_even_mac.s
@@ -0,0 +1,27 @@
+# Compile using gcc -g -nostdlib odd_even.s
+# Check if a number N is odd or even
+# If even store 2 in rbx, else store 3 in rbx
+
+.global _main
+
+.text
+
+_main:
+        mov $15, %rax        # N = 15
+        and $1, %rax         # N = N&1
+        cmp $0, %rax         # Compare value of rax with 0
+        je even
+        jne odd
+
+even: 
+        mov $2, %rbx
+        jmp exit
+
+odd: 
+        mov $3, %rbx
+        jmp exit
+
+exit:
+        mov     $60, %rax               # system call 60 is exit
+        xor     %rdi, %rdi              # we want return code 0
+        syscall    
